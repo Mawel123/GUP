53fda8de144b88fc5d66ad8881c14c763e35fd5a
Mawel123 <34421091+Mawel123@users.noreply.github.com>
2017-12-10 17:36:27 +0100

    Add files via upload

---
+++ Punkt.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Mawel
+ */
+public class Punkt {
+
+    double x, y;
+// Konstruktor
+
+    public Punkt(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
